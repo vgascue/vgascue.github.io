32335283aef8c60eb78bf4142d27e9b9e5957979
vgascue <vgascue@fcien.edu.uy>
2022-08-17 11:34:42 -0300

    Entrega 1-pauta 2
    Agrego listado de autos en la sección productos

---
+++ js/products.js
@@ -0,0 +1,40 @@
+let productsArray = [];
+//función que recibe un array con los datos, y los muestra en pantalla a través el uso del DOM
+function showProductsList(array){
+    let htmlContentToAppend = "";
+
+    for(let i = 0; i < array.length; i++){ 
+        let product = array[i];
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + product.image + `" alt="product image" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <div class="mb-1">
+                        <h4>`+ product.name + '-' + product.currency + String(product.cost) +`</h4> 
+                        <p> `+ product.description +`</p> 
+                        </div>
+                        <small class="text-muted">` + String(product.soldCount) + ` vendidos</small> 
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        `
+        document.getElementById("prod-list-container").innerHTML = htmlContentToAppend; 
+    }
+}
+
+
+    document.addEventListener("DOMContentLoaded", function(){
+        getJSONData(LIST_URL).then(function(resultObj){
+            if (resultObj.status === "ok")
+            {   productsArray = resultObj.data.products;
+                console.log(productsArray)
+                showProductsList(productsArray);
+            }
+        });
+    });
